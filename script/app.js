f94fb62f6dd0102e13c48b7c9f77f4d21e1ef19c
DejonckheereWard <ward.dejonckheere@student.howest.be>
2018-10-21 14:47:29 +0200

    Working on js

--- script/app.js
+++ script/app.js
@@ -1,7 +1,12 @@
 // _ = helper functions
 let _calculateTimeDistance = (startTime, endTime) => {
-	// Bereken hoeveel tijd er tussen deze twee periodes is.
-	// Tip: werk met minuten.
+	startTime = new Date('0001-01-01 ' + startTime);
+	endTime = new Date('0001-01-01 ' + endTime);
+	startTime = (startTime.getHours()*60) + startTime.getMinutes();
+	endTime = (endTime.getHours()*60) + endTime.getMinutes();
+	let distance = endTime - startTime;
+	// console.info(distance);
+	return distance;
 }
 
 // Deze functie kan een am/pm tijd omzetten naar een 24u tijdsnotatie, deze krijg je dus al. Alsjeblieft, veel plezier ermee.
@@ -15,33 +20,35 @@ let _convertTime = (t) => {
 // 5 TODO: maak updateSun functie
 
 // 4 Zet de zon op de juiste plaats en zorg ervoor dat dit iedere minuut gebeurt.
-let placeSunAndStartMoving = ( totalMinutes, sunrise ) => {
-	// In de functie moeten we eerst wat zaken ophalen en berekenen.
-
-	// Haal het DOM element van onze zon op en van onze aantal minuten resterend deze dag.
-	// Bepaal het aantal minuten dat de zon al op is.
-
-	// Nu zetten we de zon op de initiële goede positie ( met de functie updateSun ). Bereken hiervoor hoeveel procent er van de totale zon-tijd al voorbij is.
-	// We voegen ook de 'is-loaded' class toe aan de body-tag.
-	// Vergeet niet om het resterende aantal minuten in te vullen.
-
-
-	// Nu maken we een functie die de zon elke minuut zal updaten
-	// Bekijk of de zon niet nog onder of reeds onder is
-	
-	// Anders kunnen we huidige waarden evalueren en de zon updaten via de updateSun functie.
-	// PS.: vergeet weer niet om het resterend aantal minuten te updaten en verhoog het aantal verstreken minuten.
+let placeSunAndStartMoving = ( totalMinutes, sunrise, sunset ) => {
+		
 }
 
 // 3 Met de data van de API kunnen we de app opvullen
 let showResult = ( queryResponse ) => {
 	// We gaan eerst een paar onderdelen opvullen
 	// Zorg dat de juiste locatie weergegeven wordt, volgens wat je uit de API terug krijgt.
+	let data = queryResponse.query.results.channel;
+	let location = document.querySelector(".js-location");
+	let city = data.location.city;
+	let country = data.location.country;
+	location.innerHTML = city + ", " + country;
 	// Toon ook de juiste tijd voor de opkomst van de zon en de zonsondergang.
+	let sunriseHTML = document.querySelector(".js-sunrise");
+	let sunrise = _convertTime(data.astronomy.sunrise);
+	sunriseHTML.innerHTML = sunrise;
+	let sunsetHTML = document.querySelector(".js-sunset");
+	let sunset = _convertTime(data.astronomy.sunset);
+	sunsetHTML.innerHTML = sunset;
 
+	var currentDate = new Date()
+	let sunElement = document.querySelector(".js-sun");
+	sunElement.setAttribute("data-time", (currentDate.getHours() + ':' + ('0' + currentDate.getMinutes()).slice(-2)))
 
 	// Hier gaan we een functie oproepen die de zon een bepaalde postie kan geven en dit kan updaten.
 	// Geef deze functie de periode tussen sunrise en sunset mee en het tijdstip van sunrise.
+	let timeDistance = _calculateTimeDistance(sunrise, sunset);
+	placeSunAndStartMoving(timeDistance, sunrise, sunset);
 };
 
 // 2 Aan de hand van een longitude en latitude gaan we de yahoo wheater API ophalen.
@@ -51,9 +58,28 @@ let getAPI = ( lat, lon ) => {
 
 	// Met de fetch API proberen we de data op te halen.
 	// Als dat gelukt is, gaan we naar onze showResult functie.
-}
+	fetch('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text%3D%22(' + lat + '%2C' + lon + ')%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+				showResult(data);
+      });
+
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  })};
 
 document.addEventListener( 'DOMContentLoaded', function () {
 	// 1 We will query the API with longitude and latitude.
-	getAPI( 50.8027841, 3.2097454 );
+	getAPI( 50.825888, 3.2507454 );
 });
